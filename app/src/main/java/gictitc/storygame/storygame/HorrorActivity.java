77875f8b657cebdea8064a032b08c97738b6ec98
Panha <sovannpanha164@gmail.com>
2018-08-08 15:26:13 +0700

    To create Menu for chossing Story Type

---
+++ app/src/main/java/gictitc/storygame/storygame/HorrorActivity.java
@@ -0,0 +1,16 @@
+package gictitc.storygame.storygame;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HorrorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_horror);
+    }
+
+}
--- app/src/main/java/gictitc/storygame/storygame/MainActivity.java
+++ app/src/main/java/gictitc/storygame/storygame/MainActivity.java
@@ -3,14 +3,27 @@ package gictitc.storygame.storygame;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public abstract class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Button bHorror,bDrama,bSciFi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bHorror=findViewById(R.id.bHorror);
+        bHorror.setOnClickListener(this);
+
+        bDrama=findViewById(R.id.bDrama);
+        bDrama.setOnClickListener(this);
+
+        bSciFi=findViewById(R.id.bSciFi);
+        bSciFi.setOnClickListener(this);
     }
+
+
+
 
 
 }
